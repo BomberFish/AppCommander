b2d0784f44f5b00612b726d1376df73779c681b1
BomberFish <bomberfish@bomberfish.ca>
2023-12-21 18:33:46 -0500

    Initial Commit

---
+++ AppCommander/AppCommanderApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// AppCommanderApp.swift – AppCommander
+// created on 2023-12-21
+
+import SwiftUI
+
+@main
+struct AppCommanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCommander/ContentView.swift
@@ -0,0 +1,21 @@
+// bomberfish
+// ContentView.swift – AppCommander
+// created on 2023-12-21
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
